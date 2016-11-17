08e9ddcd7d093b76507bd577323c5cc7a9ac14db
hamadu <dercll1020@gmail.com>
2016-11-18 00:20:25 +0900

    fast connect

--- src/js/components/World.tsx
+++ src/js/components/World.tsx
@@ -37,18 +37,91 @@ export class World extends React.Component<WorldProps, WorldState> {
   }
 
   connect() {
-    const threshold = this.props.width * this.props.height / 20;
-    let e: number[][] = [];
-    const n = this.state.vertice.length;
-    for (let i = 0 ; i < n ; i++) {
+    let bucket: number[][][];
+
+    bucket = [
+      [[], [], [], []],
+      [[], [], [], []],
+      [[], [], [], []],
+      [[], [], [], []]
+    ];
+
+    for (let i = 0 ; i < this.props.n ; i++) {
       const v1 = this.state.vertice[i];
-      for (let j = i+1 ; j < n ; j++) {
-        const v2 = this.state.vertice[j];
-        const dx = (v1.x-v2.x)|0;
-        const dy = (v1.y-v2.y)|0;
-        const d = Math.pow(dx, 2) + Math.pow(dy, 2);
-        if (d < threshold) {
-          e.push([i, j]);
+      const xpart = Math.max(0, Math.min(3, Math.floor(v1.x * 4 / this.props.width)));
+      const ypart = Math.max(0, Math.min(3, Math.floor(v1.y * 4 / this.props.height)));
+      bucket[xpart][ypart].push(i);
+    }
+
+    const threshold = this.props.width * this.props.height / 20;
+
+    let e: number[][] = [];
+    for (let xi = 0 ; xi <= 3 ; xi++) {
+      for (let yi = 0 ; yi <= 3 ; yi++) {
+        const set0 = bucket[xi][yi];
+        const n = set0.length;
+        for (let i = 0 ; i < n ; i++) {
+          const v1 = this.state.vertice[set0[i]];
+          for (let j = i+1 ; j < n ; j++) {
+            const v2 = this.state.vertice[set0[j]];
+            const dx = (v1.x-v2.x)|0;
+            const dy = (v1.y-v2.y)|0;
+            const d = Math.pow(dx, 2) + Math.pow(dy, 2);
+            if (d < threshold) {
+              e.push([set0[i], set0[j]]);
+            }
+          }
+        }
+
+        if (xi + 1 < bucket.length) {
+          const set1 = bucket[xi+1][yi];
+          const m = set1.length;
+          for (let i = 0 ; i < n ; i++) {
+            const v1 = this.state.vertice[set0[i]];
+            for (let j = 0 ; j < m ; j++) {
+              const v2 = this.state.vertice[set1[j]];
+              const dx = (v1.x-v2.x)|0;
+              const dy = (v1.y-v2.y)|0;
+              const d = Math.pow(dx, 2) + Math.pow(dy, 2);
+              if (d < threshold) {
+                e.push([set0[i], set1[j]]);
+              }
+            }
+          }
+        }
+
+        if (yi + 1 < bucket[0].length) {
+          const set1 = bucket[xi][yi+1];
+          const m = set1.length;
+          for (let i = 0 ; i < n ; i++) {
+            const v1 = this.state.vertice[set0[i]];
+            for (let j = 0 ; j < m ; j++) {
+              const v2 = this.state.vertice[set1[j]];
+              const dx = (v1.x-v2.x)|0;
+              const dy = (v1.y-v2.y)|0;
+              const d = Math.pow(dx, 2) + Math.pow(dy, 2);
+              if (d < threshold) {
+                e.push([set0[i], set1[j]]);
+              }
+            }
+          }
+        }
+
+        if (xi + 1 < bucket.length && yi + 1 < bucket[0].length) {
+          const set1 = bucket[xi+1][yi+1];
+          const m = set1.length;
+          for (let i = 0 ; i < n ; i++) {
+            const v1 = this.state.vertice[set0[i]];
+            for (let j = 0 ; j < m ; j++) {
+              const v2 = this.state.vertice[set1[j]];
+              const dx = (v1.x-v2.x)|0;
+              const dy = (v1.y-v2.y)|0;
+              const d = Math.pow(dx, 2) + Math.pow(dy, 2);
+              if (d < threshold) {
+                e.push([set0[i], set1[j]]);
+              }
+            }
+          }
         }
       }
     }
@@ -56,12 +129,12 @@ export class World extends React.Component<WorldProps, WorldState> {
   }
 
   render() {
-    const v = this.state.vertice.map(v => <Vertex x={v.x} y={v.y} r={3} />)
+    const v = this.state.vertice.map(v => <Vertex x={v.x} y={v.y} r={v.r} />)
     const n = this.state.vertice.length;
     const e = this.state.edges.map(vids => {
       const v1 = this.state.vertice[vids[0]];
       const v2 = this.state.vertice[vids[1]];
-      <Line x1={v1.x} y1={v1.y} x2={v2.x} y2={v2.y} />
+      return <Line x1={v1.x} y1={v1.y} x2={v2.x} y2={v2.y} />
     });
 
     return (
--- src/js/model/VertexData.tsx
+++ src/js/model/VertexData.tsx
@@ -1,21 +1,27 @@
 import { World } from '../components/World'
 
-export interface VertexData { x: number, y: number, vx: number, vy: number }
+export interface VertexData { x: number, y: number, r: number, vx: number, vy: number }
 
 export class VertexData {
-  constructor(x: number, y: number, vx: number, vy: number) {
+  constructor(x: number, y: number, r: number, vx: number, vy: number) {
     this.x = x;
     this.y = y;
+    this.r = r;
     this.vx = vx;
     this.vy = vy;
   }
 
   static generate(world: World) {
+    const size = 2 + Math.random() * 4;
+    const R = size / 3;
+    const theta = Math.random() * Math.PI * 2;
+
     return new VertexData(
       (world.props.width * Math.random() | 0),
       (world.props.height * Math.random() | 0),
-      4 * Math.random() - 2,
-      4 * Math.random() - 2,
+      size,
+      R * Math.cos(theta),
+      R * Math.sin(theta)
     )
   }
 
@@ -26,6 +32,6 @@ export class VertexData {
     const toVX = (toX - padding < 0 || toX + padding >= world.props.width)  ? -this.vx : this.vx;
     const toVY = (toY - padding < 0 || toY + padding >= world.props.height) ? -this.vy : this.vy;
 
-    return new VertexData(toX, toY, toVX, toVY);
+    return new VertexData(toX, toY, this.r, toVX, toVY);
   }
 }
